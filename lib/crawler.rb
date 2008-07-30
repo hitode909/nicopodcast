320103269f5bbeadfbe281cbd6e67ac8bfe53a34
hitode909 <hitode909@gmail.com>
2008-07-30 23:13:20 +0900

    忘れそうだったのでコメント追加

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -5,15 +5,20 @@ require 'rubygems'
 require 'nicovideo'
 require 'yaml'
 
+$KCODE = 'UTF8'
+
 class Crawler
   attr_writer :input_feed_url, :output_feed_path, :output_file_path, :output_file_url
-  
+
+  # ここでは設定だけ読んでおく
   def initialize
     collect_input_data
   end
 
+  # クローラーを実行する(fetch feed, download, encode, publish)
   def run
     setup_input_data
+    get_feed
     get_input_files
     sync_file_sizes
     get_output_files
@@ -24,6 +29,8 @@ class Crawler
     publish_feed
   end
 
+  # 継承したクラスでここに設定をいろいろ書く
+  # writerがあるので、あとから書き換え可
   def collect_input_data
     @input_feed_url = ''
     @output_feed_path = ''
@@ -34,12 +41,16 @@ class Crawler
     @ffmpeg_option = ''
   end
 
+  # フィードを出力するときに呼ばれる
+  # ここでタイトルを見やすいように置換したりする
   def hook_publish_feed
     @output_feed.items.each do |item|
       item.title.strip!
     end
   end
   
+  # 設定をあとで使いやすいように整形
+  # (相対パスを絶対パスに直したり, )
   def setup_input_data
     @account_setting_file = File.expand_path(@account_setting_file)
     @input_file_type.gsub!(/^\./,'')
@@ -47,15 +58,18 @@ class Crawler
     @output_feed_path = File.expand_path(@output_feed_path)
     @output_file_path = File.expand_path(@output_file_path)
     @output_file_url.gsub!(/\/$/,'')
-    
-    @input_feed = get_feed
+  end
 
+  # フィードを取得してついでに関連情報を格納する
+  def get_feed
+    @input_feed = download_feed
     @video_keys = get_video_keys
     @file_sizes = get_file_sizes
     @video_titles = get_video_titles
   end
 
-  def get_feed
+  # フィードをwgetで取ってくる
+  def download_feed
     puts "downloading feed(#{@input_feed_url})"
     tmpfile = "/tmp/nicopodcast#{@input_feed_url.scan(/\d+/).first}"
     system "wget -nv -O #{tmpfile} #{@input_feed_url}"
@@ -64,6 +78,7 @@ class Crawler
     return rss
   end
 
+  # フィードからビデオのidのArrayを得る
   def get_video_keys
     list = []
     @input_feed.items.each do |item|
@@ -74,6 +89,7 @@ class Crawler
     return list
   end
 
+  # フィードからビデオのidとタイトルのHashを得る
   def get_video_titles
     hash = {}
     @input_feed.items.each do |item|
@@ -84,10 +100,15 @@ class Crawler
     return hash
   end
 
+  # ローカルのファイルサイズのHashを@file_sizesに入れる
   def sync_file_sizes
     @file_sizes = get_file_sizes
   end
-  
+
+  # ローカルのinput,outputのファイルサイズのHashを得る
+  # hash['sm0000']['flv'] = 4000
+  # hash['sm0000']['mp3'] = nil
+  # みたいなかんじになる
   def get_file_sizes
     hash = {}
     @video_keys.each do |key|
@@ -98,15 +119,23 @@ class Crawler
     end
     return hash
   end
-  
+
+  # 出力ファイルのフルパスを得る
+  # key: 'sm00000'とか
+  # type: 'mp3'とか
   def output_file_fullpath(key,type)
     return @output_file_path + '/' + key + '.' + type
   end
-  
+
+  # 出力ファイルのURLを得る
+  # key: 'sm00000'とか
+  # type: 'mp3'とか
   def output_file_fullurl(key,type)
     return @output_file_url + '/' + key + '.' + type
   end
-  
+
+  # ダウンロードすべき動画を調べてダウンロードする
+  # 例えば、flvもmp3もないとき、ダウンロードするべき
   def get_input_files
     keys = @file_sizes.delete_if { |key,value|
       value[@input_file_type] or value[@output_file_type]
@@ -121,8 +150,9 @@ class Crawler
     return
   end
 
-  ## this method was made from nv_download (gem nicovideo)
-  ## http://rubyforge.org/projects/nicovideo/
+  # 指定された動画たちをダウンロードする
+  # gem nicovideoのサンプル(nv_download)を一部コピー
+  # http://rubyforge.org/projects/nicovideo/
   def download(video_ids)
     puts "download: #{video_ids.join(', ')}"
     
@@ -155,6 +185,9 @@ class Crawler
     return
   end
 
+  # エンコードすべき動画を調べてエンコードする
+  # (エンコードとは、flvをmp3とかにする作業)
+  # 例えば、flvがあって、mp3がないとき、エンコードするべき
   def get_output_files
     keys = @file_sizes.select { |key,value|
       value[@input_file_type] and not value[@output_file_type]
@@ -169,6 +202,7 @@ class Crawler
     return
   end
 
+  # ffmpegでinput_fileをoutput_fileにエンコードする
   def encode(video_ids)
     puts "encode: #{video_ids.join(', ')}"
     video_ids.each do |video_id|
@@ -181,6 +215,7 @@ class Crawler
     return
   end
 
+  # input_fileもouput_fileもあるときinput_fileを削除する
   def delete_no_need_files
     keys = @file_sizes.select { |key,value|
       value[@input_file_type] and value[@output_file_type]
@@ -199,6 +234,9 @@ class Crawler
     return
   end
 
+  # フィードを生成する
+  # 新しいフィードを作って、input_feedから要素をコピーして、
+  # enclosureをつけたりする
   def generate_feed
     puts "generating feed"
     @output_feed = RSS::Maker.make("2.0") do |maker|
@@ -234,7 +272,8 @@ class Crawler
       end
     end
   end
-  
+
+  # フィードを書き出す
   def publish_feed
     puts "writing feed at #{@output_feed_path}"
     if File.size?(@output_feed_path)
